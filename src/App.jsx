9778a6a3eac5d197607eeb5169e2a7eae0cbae89
Chathushi <chathushikavindya09@gmail.com>
2024-03-31 08:23:06 +0530

    remove unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,10 @@ import ResetEmail from "./pages/ResetEmail.jsx";
 import UserProfile from "./pages/UserProfile.jsx";
 import ChangePassword from "./pages/ChangePassword.jsx";
 import Notification from "./pages/Notification.jsx";
-import AddVehicleDetails from "./pages/AddVehicleDetails.jsx";
 import AddVehicleMaintenanceDetails from "./pages/AddVehicleMaintenanceDetails.jsx";
 import AddFuelRefillDetails from "./pages/AddFuelRefillDetails.jsx";
-import Driver from "./pages/Driver.jsx";
-import Helper from "./pages/Helper.jsx";
-import Staff from "./pages/Staff.jsx";
-import Trip from "./pages/Trip.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
-import AddAccidentDetails from "./pages/AddAccidentDetails.jsx";
-import AddVehicleModel from "./pages/AddVehicleModel.jsx";
-import AddManufactureDetails from "./pages/AddManufactureDetails.jsx";
-import AddVehicleType from "./pages/AddVehicleType.jsx";
 import AddMaintenanceType from "./pages/AddMaintenanceType.jsx";
-import VehicleDetailsTable from "./pages/VehicleDetailsTable.jsx";
-import VehicleTypeTable from "./pages/VehicleTypeTable.jsx";
-import VehicleModelTable from "./pages/VehicleModelTable.jsx";
-import ManufactureTable from "./pages/ManufatureTable.jsx";
 import MaintenanceTable from "./pages/MaintenanceTable.jsx";
 import MaintenanceTypeTable from "./pages/MaintenanceTypeTable.jsx";
 import FuelRefillTable from "./pages/FuelRefillTable.jsx";
@@ -38,24 +25,10 @@ export default function App() {
                 <Route path="Dashboard" element={<Dashboard/>}/>
                 <Route path="UserProfile" element={<UserProfile/>}/>
                 <Route path="Notification" element={<Notification/>}/>
-                <Route path="AddVehicleDetails" element={<AddVehicleDetails/>}/>
                 <Route path="AddVehicleMaintenanceDetails" element={<AddVehicleMaintenanceDetails/>}/>
                 <Route path="AddFuelRefillDetails" element={<AddFuelRefillDetails/>}/>
-                <Route path="Driver" element={<Driver/>}/>
-                <Route path="Helper" element={<Helper/>}/>
-                <Route path="Staff" element={<Staff/>}/>
-                <Route path="Trip" element={<Trip/>}/>
-                <Route path="AddAccidentDetails" element={<AddAccidentDetails/>}/>
-                <Route path="AddAccidentDetails" element={<AddAccidentDetails/>}/>
                 <Route path="ChangePassword" element={<ChangePassword/>}/>
-                <Route path="AddVehicleType" element={<AddVehicleType/>}/>
-                <Route path="AddVehicleModel" element={<AddVehicleModel/>}/>
-                <Route path="AddManufactureDetails" element={<AddManufactureDetails/>}/>
                 <Route path="AddMaintenanceType" element={<AddMaintenanceType/>}/>
-                <Route path="VehicleDetailsTable" element={<VehicleDetailsTable/>}/>
-                <Route path="VehicleTypeTable" element={<VehicleTypeTable/>}/>
-                <Route path="VehicleModelTable" element={<VehicleModelTable/>}/>
-                <Route path="ManufactureTable" element={<ManufactureTable/>}/>
                 <Route path="MaintenanceTable" element={<MaintenanceTable/>}/>
                 <Route path="MaintenanceTypeTable" element={<MaintenanceTypeTable/>}/>
                 <Route path="FuelRefillTable" element={<FuelRefillTable/>}/>
