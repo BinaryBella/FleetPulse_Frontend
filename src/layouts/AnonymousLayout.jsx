9ebbdce2766228b3603430988bff7a3e32e29622
Chathushi <chathushikavindya09@gmail.com>
2024-04-01 14:41:46 +0530

    add comments for layouts

--- src/layouts/AnonymousLayout.jsx
+++ src/layouts/AnonymousLayout.jsx
@@ -1,13 +1,19 @@
-import Logo from './../assets/images/Logo.jpg';
-import {Outlet} from "react-router-dom";
+import Logo from './../assets/images/Logo.jpg'; // Importing the Logo image
+import { Outlet } from "react-router-dom"; // Importing the Outlet component from react-router-dom
 
+// AnonymousLayout component definition
 export default function AnonymousLayout() {
+    // Rendering the layout structure
     return (
         <div className="flex w-screen h-screen">
+            {/* Left side containing the logo */}
             <div className="bg-[#393970] w-1/2 flex items-center justify-center">
                 <img src={Logo} alt="Logo" className="w-1/2"/>
             </div>
+
+            {/* Right side for main content */}
             <div className="bg-[#E1E4EB] w-1/2 flex flex-col items-center justify-center">
+                {/* Outlet for rendering nested routes */}
                 <Outlet/>
             </div>
         </div>
